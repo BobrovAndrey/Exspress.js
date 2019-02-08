e2615a19a4a65b4101d06f6c75bf959eceb16335
Andrey Bobrov <Bobrov.law@gmail.com>
2019-02-08 10:13:19 +0200

    first server start

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('Hello world!');
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+app.get('/', function (req, res, next) {
+    res.send('Tour Booking API');
+});
+app.listen(process.env.PORT || 8091, function () { return console.log('Server started...'); });
--- server.ts
+++ server.ts
@@ -1,2 +1,8 @@
 import express = require('express')
 const app = express();
+
+
+app.get('/', (req, res, next) => {
+  res.send('Tour Booking API')
+})
+app.listen(process.env.PORT || 8091, () => console.log('Server started...'))
