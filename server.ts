593a3f9d290b231c91af942320b9979c1aa460d9
Andrey Bobrov <Bobrov.law@gmail.com>
2019-02-08 10:39:46 +0200

    automated building added

--- server.ts
+++ server.ts
@@ -5,4 +5,4 @@ const app = express();
 app.get('/', (req, res, next) => {
   res.send('Tour Booking API')
 })
-app.listen(process.env.PORT || 8091, () => console.log('Server started...'))
+app.listen(process.env.PORT || 8091, () => console.log('Server started...TEST'))
