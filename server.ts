b05fcd99e2b9d2a6383cba1453f1b37260970787
Andrey Bobrov <Bobrov.law@gmail.com>
2019-02-08 11:04:50 +0200

    get and post path added

--- server.ts
+++ server.ts
@@ -5,4 +5,13 @@ const app = express();
 app.get('/', (req, res, next) => {
   res.send('Tour Booking API')
 })
+
+app.get('/tours', (req, res, next) => {
+  res.send('Geta a list of tours...')
+})
+
+app.post('/tours', (req, res, next) => {
+  res.send('Add a new tour....')
+})
+
 app.listen(process.env.PORT || 8091, () => console.log('Server started...TEST'))
