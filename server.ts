aa940ad1808c162acaf1c27941d1aad83bf2979a
Andrey Bobrov <Bobrov.law@gmail.com>
2019-02-08 16:19:01 +0200

    import method changed

--- server.ts
+++ server.ts
@@ -1,17 +1,17 @@
-import express = require('express')
+import * as express from "express";
 const app = express();
 
 
-app.get('/', (req, res, next) => {
-  res.send('Tour Booking API')
-})
+app.get("/", (req, res, next) => {
+    res.send("TourBooking API");
+});
 
-app.get('/tours', (req, res, next) => {
-  res.send('Geta a list of tours...')
-})
+app.get("/tours", (req, res, next) => {
+    res.send("Get a list of tours");
+});
 
-app.post('/tours', (req, res, next) => {
-  res.send('Add a new tour....')
-})
+app.post("/tours", (req, res, next) => {
+    res.send("Post a new tour");
+});
 
-app.listen(process.env.PORT || 8091, () => console.log('Server started...TEST'))
+app.listen(process.env.PORT || 8091, () => console.log("Server Started..."));
